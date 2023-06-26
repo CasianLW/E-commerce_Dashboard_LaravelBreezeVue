ff7bc5caa609d2fc2084890fec86265af7048ce2
CasianLW <ciorbacasian@gmail.com>
2023-06-26 17:18:13 +0200

    services for api (needed in stores)

---
+++ resources/js/Services/api.js
@@ -0,0 +1,207 @@
+import axios from "axios";
+// import { useNotificationStore } from "@/Stores/NotifStore";
+
+const API_BASE_URL = `${import.meta.env.VITE_APP_API_URL}`;
+
+axios.defaults.headers.common["Content-Type"] = "multipart/form-data";
+axios.defaults.headers.common["Accept"] = "application/json";
+
+const authApi = axios.create({
+    baseURL: `${API_BASE_URL}/auth`,
+});
+
+authApi.interceptors.request.use(
+    (config) => {
+        let token =
+            localStorage.getItem("token") ||
+            document.cookie.replace(
+                /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
+                "$1"
+            );
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error).then((r) => console.log(r));
+    }
+);
+
+const adminApi = axios.create({
+    baseURL: `${API_BASE_URL}/admin`,
+});
+
+adminApi.interceptors.request.use(
+    (config) => {
+        let token =
+            localStorage.getItem("token") ||
+            document.cookie.replace(
+                /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
+                "$1"
+            );
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error).then((r) => console.log(r));
+    }
+);
+
+const profileApi = axios.create({
+    baseURL: `${API_BASE_URL}/profile`,
+});
+
+profileApi.interceptors.request.use(
+    (config) => {
+        let token =
+            localStorage.getItem("token") ||
+            document.cookie.replace(
+                /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
+                "$1"
+            );
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error).then((r) => console.log(r));
+    }
+);
+
+const storeApi = axios.create({
+    baseURL: `${API_BASE_URL}/store`,
+});
+
+storeApi.interceptors.request.use(
+    (config) => {
+        let token =
+            localStorage.getItem("token") ||
+            document.cookie.replace(
+                /(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/,
+                "$1"
+            );
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        Promise.reject(error).then((r) => console.log(r));
+    }
+);
+
+//
+//
+//
+//
+export default {
+    // user : async () => adminApi.get('/').then((r) => r.data).catch((e) => useNotificationStore().displayError(e.response.data.message)),
+    user: async () =>
+        adminApi
+            .get("/")
+            .then((r) => r.data)
+            .catch((e) => console.log("error get admin")),
+    getUsers: async () =>
+        adminApi
+            .get(`/users`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get users")),
+    userById: async (id, data) =>
+        adminApi
+            .get(`/users/${id}`, data)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get user")),
+    createUser: async (data) =>
+        adminApi
+            .put(`/users`, data)
+            .then((r) => r.data)
+            .catch((e) => console.log("error creating user")),
+    editUserById: async (id) =>
+        adminApi
+            .put(`/users/${id}`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error edit user")),
+    deleteUserById: async (id) =>
+        adminApi
+            .get(`/users/${id}`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get user")),
+    //
+    // Methodes pour les actualités
+    //
+    getEvents: async () =>
+        adminApi
+            .get(`/events`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get events")),
+    eventById: async (id, data) =>
+        adminApi
+            .get(`/events/${id}`, data)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get event")),
+    createEvent: async (data) =>
+        adminApi
+            .put(`/events`, data)
+            .then((r) => r.data)
+            .catch((e) => console.log("error creating event")),
+    editEventById: async (id) =>
+        adminApi
+            .put(`/events/${id}`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error edit event")),
+    deleteEventById: async (id) =>
+        adminApi
+            .get(`/events/${id}`)
+            .then((r) => r.data)
+            .catch((e) => console.log("error get event")),
+};
+
+// async fetchNews() {
+//     const token = localStorage.getItem("token");
+//     const response = await axios.get(`${API_URL}/admin/events`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     this.news = response.data;
+// },
+// async createNews(data) {
+//     const token = localStorage.getItem("token");
+//     await axios.post(`${API_URL}/admin/events`, data, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     this.fetchNews();
+// },
+// async getNews(id) {
+//     const token = localStorage.getItem("token");
+//     const response = await axios.get(`${API_URL}/admin/events/${id}`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     return response.data;
+// },
+// async editNews(id, data) {
+//     const token = localStorage.getItem("token");
+//     await axios.put(`${API_URL}/admin/events/${id}`, data, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     this.fetchNews();
+// },
+// async deleteNews(id) {
+//     const token = localStorage.getItem("token");
+//     await axios.delete(`${API_URL}/admin/events/${id}`, {
+//         headers: {
+//             Authorization: `Bearer ${token}`,
+//         },
+//     });
+//     this.fetchNews();
+// },
