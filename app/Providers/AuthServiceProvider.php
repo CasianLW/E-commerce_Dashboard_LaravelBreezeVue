9517a084469a1fc68cbeb7baaec3b315c33c98ab
CasianLW <ciorbacasian@gmail.com>
2023-06-26 16:51:50 +0200

    dev service provider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,24 +3,28 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Guards\CustomTokenGuard;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
     /**
-     * The model to policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
-    protected $policies = [
-        //
-    ];
-
-    /**
      * Register any authentication / authorization services.
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Auth::extend('custom_token', function ($app, $name, array $config) {
+            $provider = $app->make(\App\Providers\CustomUserProvider::class, ['authService' => $app->make(\App\Services\AuthService::class)]);
+            $request = $app['request'];
+            return new CustomTokenGuard($provider, $request);
+        });
+
+        Auth::provider('custom', function ($app, array $config) {
+            return new CustomUserProvider($app->make(\App\Services\AuthService::class));
+        });
     }
+    
 }
